d41967d97a1dde8949fec4b0ad597a499e417dc1
ninjinx <responsura@gmail.com>
2024-05-28 01:14:42 +0900

    3つ結合したら削除

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -8,6 +8,9 @@ public class Block : MonoBehaviour
     [SerializeField]
     private bool canRotate = true;
 
+    // 衝突中の同じ種類のオブジェクト一覧 
+    private List<GameObject> collisionObjects = new List<GameObject>();
+
     // 移動
     public void Move(Vector3 moveDirection)
     {
@@ -53,6 +56,40 @@ public class Block : MonoBehaviour
         if (canRotate)
         {
             transform.Rotate(0, 0, 90);
+        }
+    }
+
+    // 衝突時の判定
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // 同じプレハブ同士が衝突したときリストに存在しない場合
+        if (collision.gameObject.name == gameObject.name && !collisionObjects.Contains(collision.gameObject))
+        {
+            collisionObjects.Add(collision.gameObject);
+        }
+
+        // 衝突中の同じ種類のオブジェクト一覧が2つ以上のとき
+        if (collisionObjects.Count >= 2)
+        {
+            // 全てのオブジェクトを削除
+            foreach (GameObject obj in collisionObjects.ToArray())
+            {
+                Destroy(obj);
+            }
+
+            // 自身も削除
+            Destroy(gameObject);
+        }
+    }
+
+    // 分離時の判定
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        // 同じプレハブ同士が分離したときにリストに存在する場合
+        if (collision.gameObject.name == gameObject.name && collisionObjects.Contains(collision.gameObject))
+        {
+            // 衝突中のオブジェクト一覧から削除
+            collisionObjects.Remove(collision.gameObject);
         }
     }
 }
